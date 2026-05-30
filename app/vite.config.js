7e43e76f108d0756186d891181bce61c4d819409
dolua <dolua@DESKTOP-SI3J0NB>
2026-05-30 16:55:35 +0700

    fixbug

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Road-Map-Unity/',
+  base: './',
   root: '.',
   build: {
     outDir: 'dist',
